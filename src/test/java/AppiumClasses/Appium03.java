0438e5c132ba4f85f4bbc1e61aa2ef8c85b3107a
cevik <suleymancevik64@gmail.com>
2022-12-18 16:38:32 +0300

    22-12-18

--- src/test/java/AppiumClasses/Appium03.java
+++ src/test/java/AppiumClasses/Appium03.java
@@ -4,6 +4,7 @@ package AppiumClasses;
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.remote.MobileCapabilityType;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -11,17 +12,12 @@ import org.testng.annotations.Test;
 import java.net.MalformedURLException;
 import java.net.URL;
 
-public class Appium02 {
+public class Appium03 {
 
     @Test
     public void test01() throws MalformedURLException, InterruptedException {
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
-
-        /*
-        adb shell
-        dumpsys window | grep -E "mCurrentFocus"
-         */
 
         capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
         capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "13.0");
@@ -30,28 +26,27 @@ public class Appium02 {
         capabilities.setCapability(MobileCapabilityType.APP, "C:\\Users\\cevik\\IdeaProjects\\mobileTesting\\src\\Apps\\gestureTool.apk");
         capabilities.setCapability("appPackage", "com.davemac327.gesture.tool");
         capabilities.setCapability("appActivity", "com.davemac327.gesture.tool.GestureBuilderActivity");
-        capabilities.setCapability("noReset", true);  // Eger applikasyona izinleri atlayip ana sayfada acilmasini istemiyorsaniz asagidaki komutu kullanabilirsiniz
+        capabilities.setCapability("noReset", true);// Eger applikasyona izinleri atlayip ana sayfada acilmasini istemiyorsaniz asagidaki komutu kullanabilirsiniz
 
 
         AndroidDriver<MobileElement> driver = new AndroidDriver<MobileElement>(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
-
-        if (driver.isDeviceLocked()) driver.unlockDevice(); // ekran kilitliyse ekrani ac
-
         System.out.println("app yuklendi");
         Thread.sleep(2000);
         MobileElement homeScreenTitle = driver.findElementById("android:id/title");
         Assert.assertTrue(homeScreenTitle.isDisplayed());
         System.out.println("anasayfa acildi");
 
+
+        MobileElement addGesture = driver.findElementById("com.davemac327.gesture.tool:id/addButton");
+        addGesture.click();
         Thread.sleep(2000);
-        MobileElement testButton = driver.findElementById("com.davemac327.gesture.tool:id/testButton");
-        testButton.click();
-        System.out.println("test button calisti");
+
+
+        MobileElement gestureNameBox = driver.findElementById("com.davemac327.gesture.tool:id/gesture_name");
+        gestureNameBox.sendKeys("appium", Keys.ENTER);
 
         Thread.sleep(2000);
-        MobileElement testAGestureTitle = driver.findElementById("android:id/title");
-        Assert.assertTrue(testAGestureTitle.isDisplayed());
-        System.out.println("Test Screen Acildi");
+
 
         driver.closeApp();
     }
---
+++ src/test/java/AppiumClasses/Appium04.java
@@ -0,0 +1,87 @@
+package AppiumClasses;
+
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.remote.MobileCapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class Appium04 {
+
+    @Test
+    public void test() throws MalformedURLException, InterruptedException {
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+
+        capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
+        capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "13.0");
+        capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "emulator-5554");
+        capabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, "UiAutomator2");
+        capabilities.setCapability(MobileCapabilityType.APP, "C:\\Users\\cevik\\IdeaProjects\\mobileTesting\\src\\Apps\\Calculator.apk");
+        capabilities.setCapability("noReset", true);
+
+        /*
+        {
+  "automationName": "UiAutomator2",
+  "platformName": "Android",
+  "platformVersion": "13.0",
+  "app": "C:\\Users\\cevik\\IdeaProjects\\mobileTesting\\src\\Apps\\Calculator.apk",
+  "noReset": true
+}
+         */
+
+        AndroidDriver<MobileElement> driver = new AndroidDriver<MobileElement>(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+
+        MobileElement num1 = driver.findElementById("com.google.android.calculator:id/digit_1");
+        MobileElement num2 = driver.findElementById("com.google.android.calculator:id/digit_2");
+        MobileElement num3 = driver.findElementById("com.google.android.calculator:id/digit_3");
+        MobileElement num4 = driver.findElementById("com.google.android.calculator:id/digit_4");
+        MobileElement num5 = driver.findElementById("com.google.android.calculator:id/digit_5");
+        MobileElement num6 = driver.findElementById("com.google.android.calculator:id/digit_6");
+        MobileElement num7 = driver.findElementById("com.google.android.calculator:id/digit_7");
+        MobileElement num8 = driver.findElementById("com.google.android.calculator:id/digit_8");
+        MobileElement num9 = driver.findElementById("com.google.android.calculator:id/digit_9");
+        MobileElement num0 = driver.findElementById("com.google.android.calculator:id/digit_0");
+
+
+        MobileElement plus = driver.findElementByAccessibilityId("plus");
+        MobileElement multiply = driver.findElementByAccessibilityId("multiply");
+        MobileElement divide = driver.findElementByAccessibilityId("divide");
+        MobileElement minus = driver.findElementByAccessibilityId("minus");
+
+
+        MobileElement equals = driver.findElementByAccessibilityId("equals");
+
+        // 64 + 32
+
+        num6.click();
+        num4.click();
+        plus.click();
+        num3.click();
+        num2.click();
+
+        Thread.sleep(2000);
+
+        MobileElement preResult = driver.findElementById("com.google.android.calculator:id/result_preview");
+        //System.out.println("preResult = " + preResult.getText());
+
+        Thread.sleep(2000);
+        equals.click();
+
+        Thread.sleep(2000);
+        MobileElement result = driver.findElementById("com.google.android.calculator:id/result_final");
+        System.out.println("final result = " + result.getText());  //StaleElementReferenceException
+        String preResultText = result.getText();
+
+
+        Thread.sleep(2000);
+        Assert.assertEquals(result.getText(), preResultText);
+
+        Thread.sleep(2000);
+        driver.closeApp();
+    }
+}
